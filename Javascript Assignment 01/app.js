6198720a264fd1e1e8f64097981e4f4e16fdc277
Syed Muhammad Saad <136969885+SyedMuhammadSaad74@users.noreply.github.com>
2023-10-14 15:35:02 +0500

    Add files via upload

---
+++ Javascript Assignment 01/app.js
@@ -0,0 +1,144 @@
+//------------------------------CHAPTER # 01--------------------------------
+// QUESTION # 01
+// var a="Hello World";
+// alert(a);
+
+// QUESTION # 02
+// var first ="Error!Please enter a valid password.";
+// alert(first)
+
+// QUESTION 03
+// var text="Welcome to JS Land...\n Happy Coding!";
+// alert(text);
+
+// QUESTION # 04
+// var text="Welcome to JS Land";
+// alert(text);
+// var line="Happy Coding! \n Prevent this page from creating additional dialogs.";
+// alert(line);
+
+// QUESTITION # 05
+// var text="Hello... I can run JS through my web browser's console";
+// console.log(text);
+
+// QUESTION # 07
+// A
+// var text="Welcome to JS Land...\n Happy Coding!";
+//  alert(text);
+
+// B
+// var text="Welcome to JS Land...\n Happy Coding!";
+// alert(text);
+
+// C
+// var text="Welcome to JS Land...\n Happy Coding!";
+// alert(text);
+
+// D
+// var text="Welcome to JS Land...\n Happy Coding!";
+// alert(text);
+
+
+//-------------------------------CHAPTER # 02-------------------------------
+//QUESTION # 01
+//var a="username";
+//console.log(a);
+
+//QUESTION # 02
+//var myName="Syed Muhammad Saad";
+//console.log(myName);
+
+// QUESTION # 03
+// A
+// var a="message";
+// console.log(a);
+
+// B
+// var message="Hello World";
+// console.log(message);
+
+// C
+// console.log(message);
+
+// QUESTION # 04
+// var a="Jhone Doe";
+// var b="15 years old";
+// var c="Certifiied Mobile and Application Development";
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// QUESTION # 05
+// var a=" PIZZA \n PIZZ \n PIZ \n PI \n P";
+// console.log(a); 
+
+// QUESTION # 06
+// var a="My e-mail is : ";
+// var eMail="saadsyed331@gmail.com";
+// console.log(a + eMail);
+
+// QUESTION # 07
+// var a="I am trying to learn from the book";
+// var book="A smarter way to learn JavaScript";
+// console.log(a + book);
+
+// QUESTION # 08
+// var a="Yah!I can write  HTML content through JavaScript";
+// document.write(a);
+
+// QUESTION # 09
+// var a="Hello World";
+// alert(a);
+// document.write(a);
+
+//--------------------------------CHAPTER # 03-------------------------------
+// QUESTION # 01
+// var age="I am 15 years old";
+// alert(age);
+
+// QUESTION # 03
+// var a="You have visited this site";
+// var b="14 times";
+// alert(a + b);
+
+// QUESTION # 03
+// var birthYear="My birth year is 2003 <br>Data type of my declared variable is number";
+// document.write(birthYear);
+// document.write(a);
+
+// QUESTION # 04
+// var visitorsName="Saad ordered";
+// var productTitle="10 T-shirts";
+// var quantity="on J. store";
+// document.write(visitorsName+ "" + "" + productTitle+ "" + quantity);
+
+
+//-------------------------------CHAPTER # 04-------------------------------
+// QUESTION # 01
+// var firstName="Syed",middleName="Muhammad",lastName="Saad";
+// alert(firstName+middleName+lastName);
+
+// QUESTION # 02
+// ILLEGAL VARIABLE NAME
+// var 2a
+// var full name
+// var last name
+// var alert
+// var 43name
+
+// LEGAL VARIABLE NAME
+// var full_name
+// var fullName
+// var name
+// var phone
+// var name
+
+// QUESTION # 03
+// var a="Variables can only contain,numbers,$ and_.For Example:$my_1stVaraiable";
+// var b="Variable must begin  with a letter ,$ or _.For Example:$name_name or name";
+// var c="Variables names are case sensitive";
+// var d="Variables name should not be JS keywords";
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
